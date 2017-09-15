fdf9a6672492247266410cc52301d1997fc41c0f
swe1500 <598910482@qq.com>
2017-09-15 11:51:05 -0400

    Update:add 'a' to main

--- main.cpp
+++ main.cpp
@@ -1,6 +1,12 @@
 #include <stdio.h>
 
 int main(int argv, char** argc){
-printf("Hello world");
+int F[20]={0,1};
+int* a;
+*a = 5;
+for(int i=2;i<20;i++)
+F[i] = F[i-1] + F[i-2];
+
+printf("Hello world,F[20]=%d\n",F[19]);
 return 0;
 }
